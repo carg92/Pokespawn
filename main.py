d97eb89a760b2edd589718afeef506c040a69905
Cesar Romero <cromero@mopega.com.mx>
2021-08-06 00:39:07 +0000

    Se importaron mas librerias y se añadio la funcionalidad de contestar si alguien dice una de esas palabras

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import discord
 import os
+import requests
+import json
+import random
 
 client = discord.Client()
+
+#Lista de palabras para lanzar el spawn
+poke_words = ["sal", "aparece", "apurate", "legendario", "pokemones", "pokemon"]
+
+res_poke_words = [
+  "Ya van a salir...",
+  "Espera un momento...",
+  "Aqui viene un legendario, preparate...",
+  "Pendertuga, usa embolia cerebral ahora!",
+  "Pitochu, yo te elijo!"
+]
+
+#Obtener quote 
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q']+" - " + json_data[0]['a']    
+  return(quote)
+
+
 
 @client.event
 async def on_ready():
@@ -13,11 +36,17 @@ async def on_message(message):
   if message.author == client.user:
     return
 
+  msg = message.content
+
   if message.content.startswith('$spawn'):
-    await message.channel.send('Hora de spawnear pokemones!')
+    qoute = get_quote()
+    await message.channel.send(qoute)
+  
+  if any(word in msg for word in poke_words):
+    await message.channel.send(random.choice(res_poke_words))
 
 
-
+#Token del bot
 client.run(os.getenv('token'))
 
 
